93089215e99cd68cc633b52de39a74eda063c314
mittins2202 <caseyedunham@gmail.com>
2025-06-24 13:02:06 -0400

    Fix Exit Modal on Round Pages

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -405,6 +405,7 @@ const Quiz: React.FC<QuizProps> = ({ onComplete, onBack }) => {
   };
 
   const handleBackButtonClick = () => {
+    // Always show exit modal immediately, regardless of page type
     setShowExitModal(true);
   };
 
